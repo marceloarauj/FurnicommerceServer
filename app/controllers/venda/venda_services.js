ae48c4ed8f2a2f4a5319ec4e5bea959f3f2db727
Marcelo Araújo <tata-marcelo@hotmail.com>
2020-11-26 19:55:35 -0300

    Retornar ID USUARIO

--- app/controllers/venda/venda_services.js
+++ app/controllers/venda/venda_services.js
@@ -15,7 +15,7 @@ class VendaServices{
                 '${parseInt(body.uid)}',
                 '${body.movelId}',
                 ${1},
-                '${new Date().toString()}',
+                '${new Date()}',
                 '${body.imagem}'
             )`,
             (error,results)=>{
